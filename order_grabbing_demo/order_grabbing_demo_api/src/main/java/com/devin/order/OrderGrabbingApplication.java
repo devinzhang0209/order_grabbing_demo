8d86360f37383369940e57b7cc75f000f85c4787
devinzhang-vbh <zhangwei.devin@vbhledger.com>
2020-04-24 17:29:07 +0800

    包名修改

---
+++ order_grabbing_demo/order_grabbing_demo_api/src/main/java/com/devin/order/OrderGrabbingApplication.java
@@ -0,0 +1,16 @@
+package com.devin.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+
+
+@MapperScan("com.devin.order.mapper")
+@SpringBootApplication
+public class OrderGrabbingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderGrabbingApplication.class, args);
+    }
+
+}
---
+++ order_grabbing_demo/order_grabbing_demo_api/src/test/java/com/devin/order/JobSearchApplicationTests.java
@@ -0,0 +1,11 @@
+package com.devin.order;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSearchApplicationTests {
+
+    void contextLoads() {
+    }
+
+}
